d6a0280349a64f625fa4b74c208464c63f90c572
Lars Knudsen <larsgk@gmail.com>
2016-01-27 21:32:51 +0100

    Now with now events in JSON

--- gotest.go
+++ gotest.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+)
+
+type EkEvent struct {
+	Type string
+	Time int64
+}
+
+func handlerNowEvent(w http.ResponseWriter, r *http.Request) {
+	testEvent := EkEvent{"Now", time.Now().Unix()}
+
+	js, err := json.Marshal(testEvent)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
+}
 
 func main() {
-	fmt.Printf("hello, world\n")
+	fmt.Printf("Hello, json\n")
+	http.HandleFunc("/now", handlerNowEvent)
+	http.ListenAndServe(":3000", nil)
 }
