3dbd29a12d8b6148d3e2462a312aa16c3242bff0
Lars Knudsen <larsgk@gmail.com>
2016-02-13 11:44:01 +0100

    Adding mux(router and websockets) + favicon

--- gotest.go
+++ gotest.go
@@ -2,16 +2,27 @@ package main
 
 import (
 	"fmt"
+	"github.com/gorilla/mux"
 	"net/http"
 )
 
 func main() {
 	fmt.Printf("Hello, simple web service :)\n")
 
-	http.HandleFunc("/now", handleNowEvent)
-	http.HandleFunc("/commports", handleListCommPortsEvent)
+	r := mux.NewRouter()
 
-	http.Handle("/", http.FileServer(http.Dir("./static")))
+	r.HandleFunc("/favicon.png", handleFavicon)
+
+	r.HandleFunc("/now", handleNowEvent).
+		Methods("POST")
+	r.HandleFunc("/commports", handleListCommPortsEvent).
+		Methods("POST")
+
+	http.HandleFunc("/data", handleWSData)
+
+	r.PathPrefix("/").Handler(http.FileServer(http.Dir("./static/")))
+
+	http.Handle("/", r)
 
 	http.ListenAndServe(":3000", nil)
 }
--- webapi.go
+++ webapi.go
@@ -3,7 +3,12 @@ package main
 import (
 	"encoding/json"
 	// "github.com/larsgk/gotest/auth"
+	"github.com/gorilla/websocket"
 	"github.com/larsgk/gotest/comm"
+	"image"
+	"image/color"
+	"image/png"
+	"log"
 	"net/http"
 	"time"
 )
@@ -46,4 +51,45 @@ func sendJsonEvent(w http.ResponseWriter, daEvent interface{}) {
 	w.Header().Set("Content-Type", "application/json")
 	w.Header().Set("Cache-Control", "private, no-cache")
 	w.Write(js)
+}
+
+func handleFavicon(w http.ResponseWriter, r *http.Request) {
+	m := image.NewNRGBA(image.Rectangle{Min: image.Point{0, 0}, Max: image.Point{32, 32}})
+
+	// TODO: Reflect connection status or other here...
+	for y := 0; y < 32; y++ {
+		for x := 0; x < 32; x++ {
+			m.SetNRGBA(x, y, color.NRGBA{uint8(x * 8), uint8((x + y) * 8), uint8(y) * 8, 255})
+
+		}
+	}
+	w.Header().Set("Content-Type", "image/png")
+	if err := png.Encode(w, m); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+var upgrader = websocket.Upgrader{}
+
+func handleWSData(w http.ResponseWriter, r *http.Request) {
+	c, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Print("upgrade:", err)
+		return
+	}
+	defer c.Close()
+	for {
+		mt, message, err := c.ReadMessage()
+		if err != nil {
+			log.Println("read:", err)
+			break
+		}
+		log.Printf("recv: %s", message)
+		err = c.WriteMessage(mt, message)
+		if err != nil {
+			log.Println("write:", err)
+			break
+		}
+	}
 }
